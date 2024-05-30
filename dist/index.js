b692a486af81cf9a074b9020769bcf13d10c9728
mhmd <ms.mhmdsalah@gmail.com>
2024-05-30 17:29:26 +0300

    pending

--- dist/index.js
+++ dist/index.js
@@ -2,3 +2,4 @@
 let gameName = "Guess The Word";
 document.title = gameName;
 document.querySelector("h1").innerHTML = gameName;
+document.querySelector("footer").innerHTML = `${gameName} Game Created With Elzero `;
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,10 @@
 let gameName = "Guess The Word";
 document.title = gameName;
-document.querySelector("h1")!.innerHTML =gameName
+document.querySelector("h1")!.innerHTML = gameName;
+document.querySelector(
+  "footer"
+)!.innerHTML = `${gameName} Game Created With Elzero `;
+
+// settings game options
+let numberOfTries:number = 5;
+
